b0136dcff9127b817dea3924b32980f63f39feac
Rodrigo Dominguez <rodrigo.dominguez@alumnos.ucn.cl>
2021-06-10 14:40:46 -0400

    feat: form add dates

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -12,6 +12,26 @@ const Formulario = () => {
           className="u-full-width"
           placeholder="Nombre Mascota"
         />
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="dueno"
+          className="u-full-width"
+          placeholder="Nombre Dueño"
+        />
+        <label>Fecha alta</label>
+        <input type="date" name="fecha" className="u-full-width" />
+        <label>Hora alta</label>
+        <input type="time" name="hora" className="u-full-width" />
+        <label>Sintomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          placeholder="Describa los sintomas"
+        />
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
       </form>
     </Fragment>
   );
